6eb7153c5b9211c3829f27e01f79fe37c8b9c164
Daven Quinn <dev@davenquinn.com>
2022-03-03 06:19:25 -0600

    Some changes to app configuration

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,1 +1,20 @@
-from .data_import import import_earthchem, EarthChemImporter
+from pathlib import Path
+from pandas import read_table
+from IPython import embed
+import sparrow
+
+
+@sparrow.task(name="import-earthchem")
+def import_earthchem():
+    """
+    Import EarthChem data
+    """
+    data_dir = Path(sparrow.settings.DATA_DIR)
+    files = data_dir.glob("*.txt")
+    for file in files:
+        df = read_table(file, sep="\t")
+        ## The line below embeds a shell prompt in the notebook
+        embed()
+        ## Here we raise an error, so we don't get in an endless loop
+        ## of shell embedding :)
+        raise
--- sparrow-config.sh
+++ sparrow-config.sh
@@ -1,3 +1,6 @@
 export COMPOSE_PROJECT_NAME=navdat
 export SPARROW_DATA_DIR="$SPARROW_CONFIG_DIR/data"
 export SPARROW_PLUGIN_DIR="$SPARROW_CONFIG_DIR/plugins"
+
+# Disable sparrow worker subsystem
+export SPARROW_TASK_WORKER=0
