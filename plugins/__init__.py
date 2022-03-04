2560d4555ff4cdb4aa981676ad7e3a1f2e841784
Daven Quinn <dev@davenquinn.com>
2022-03-04 11:58:08 -0600

    Import samples by row

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 import pandas
+import numpy as N
 import re
 from pathlib import Path
 from IPython import embed
@@ -26,17 +27,6 @@ def import_earthchem():
             df.apply(import_sample, axis=1)
 
 
-@dataclass
-class Value:
-    """
-    A simple class to hold a value and its unit.
-    """
-
-    value: float
-    unit: str
-    method: str
-
-
 def import_sample(row):
     """
     This function imports a single sample from the EarthChem dump file.
@@ -48,17 +38,71 @@ def import_sample(row):
         uid = col_id + " UNIT"
         mid = col_id + " METH"
         if col_id + " UNIT" in row.index and col_id + " METH" in row.index:
-            val = Value(val, str(row.loc[uid]), str(row.loc[mid]))
+            val = Value(val, col_id, str(row.loc[uid]), str(row.loc[mid]))
             print(f"{col_id}: {val.value} {val.unit} ({val.method})")
+        elif col_id.endswith("AGE"):
+            v1 = float(val)
+            if v1 < 0:
+                # The EarthChem portal uses negative ages to indicate an age in years before present.
+                val = Value(v1 * -1, col_id, "year", "UNKNOWN")
+            else:
+                val = Value(v1, col_id, "Ma", "UNKNOWN")
         else:
             print(f"{col_id}: {val}")
         data[col_id] = val
 
     print(data)
+    # Now, we restructure the data into its final nested form
+
+    # Get reference data
+    ref = data["REFERENCE"]
+    year = re.search(r", (\d{4})$", ref)
+
+    if year is None:
+        raise Exception(f"Could not parse reference {ref}")
+    authors = ref[: year.start()].strip()
+    year = int(year.group(1))
+
+    sample = {
+        "name": data["SAMPLE ID"],
+        "location": {
+            "type": "Point",
+            "coordinates": [
+                float(data["LONGITUDE"]),
+                float(data["LATITUDE"]),
+            ],
+        },
+        "publication": {
+            "author": authors,
+            "year": year,
+        },
+    }
+
+    if precision := data.get("LOC PREC"):
+        sample["location_precision"] = meters_per_degree(
+            float(data["LATITUDE"])
+        ) * float(precision)
+
+    sample["sessions"] = []
+
+    print(sample)
+
     print()
 
 
 ## Utility functions
+
+
+@dataclass
+class Value:
+    """
+    A simple class to hold a value and its unit.
+    """
+
+    value: float
+    parameter: str
+    unit: str
+    method: str
 
 
 def combine_repeated_columns(df):
@@ -86,3 +130,13 @@ def duplicate_indexes(df, col_name) -> List[int]:
     for ix_1, column_name_1 in enumerate(df.columns[ix + 1 :]):
         if column_name_1 == col_name:
             yield ix_1 + ix
+
+
+def meters_per_degree(lat: float) -> float:
+    """
+    Return approximate number of meters per degree of latitude.
+    https://stackoverflow.com/questions/639695/how-to-convert-latitude-or-longitude-to-meters
+    We need this function because Sparrow stores measurement precision in meters, not degrees.
+    """
+    cos_lat = N.cos(N.radians(lat))
+    return 111132.92 - 559.82 * cos_lat ** 2 + 1.175 * cos_lat ** 4
